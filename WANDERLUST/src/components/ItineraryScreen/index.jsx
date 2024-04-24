7c2ce33e7e132758864b1fa621cccdae65f9ba3f
Jithendranath8 <145124590+Jithendranath8@users.noreply.github.com>
2024-04-24 12:58:51 +0530

    Add files via upload

---
+++ WANDERLUST/src/components/ItineraryScreen/index.jsx
@@ -0,0 +1,94 @@
+import "./style.css";
+import BrandHeader from "../Common/BrandHeader/index.jsx";
+import Card from "../Common/Card/index.jsx";
+import Button from "../Common/Button/index.jsx";
+import { Link, useLocation } from "react-router-dom";
+import useFetchAPI from "../../hooks/useFetchAPI.js";
+import Loading from "../Common/Loading/index.jsx";
+import ErrorScreen from "../ErrorScreen/index.jsx";
+import { createTripPrompt } from "../../utils/prompt.js";
+import { useEffect, useState } from "react";
+
+const ItineraryScreen = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const location = useLocation();
+  const { inputValues } = location.state || {};
+  const prompt = createTripPrompt({
+    where_to: inputValues[1],
+    number_of_days: inputValues[2],
+    trip_start: inputValues[3],
+    itinerary_type: inputValues[4],
+    budget: inputValues[5],
+    travel_preference: "",
+  });
+
+  useEffect(() => {
+    // write a fetch call to get the data from the openAI API, also dont forget to pass prompt as body :)
+    setLoading(true);
+    fetch("https://trip-planner-ai-api.onrender.com/wanderlust", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt,
+      }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        if (data.success) {
+          try {
+            const responseData = JSON.parse(data.response);
+            if (responseData.success === false) {
+              setError(responseData.error.message);
+            } else if (!responseData?.itinerary) {
+              throw new Error(Object.values(responseData)[0]);
+            } else {
+              setData(responseData);
+            }
+          } catch (error) {
+            setError(error.message);
+          }
+        }
+      })
+      .catch((error) => {
+        setError(error.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });    
+  }, []);
+
+  const itineraries = data?.itinerary || [];
+  if (data) {
+    return (
+      <div className="itinerary-screen">
+        <BrandHeader />
+        <div className="cards-wrapper">
+          {itineraries?.map((itinerary, index) => {
+            return <Card itinerary={itinerary} key={index} />;
+          })}
+        </div>
+        <Link to="/fill-details" className="back-button">
+          <Button text="Plan a new trip" />
+        </Link>
+      </div>
+    );
+  }
+  if (loading) return <Loading />;
+  if (error && !loading)
+    return (
+      <div>
+        <ErrorScreen errorMessage={error} />
+      </div>
+    );
+};
+
+export default ItineraryScreen;
---
+++ WANDERLUST/src/utils/prompt.js
@@ -0,0 +1,261 @@
+const JSON_FORMAT = JSON.stringify({
+  type: "object",
+  properties: {
+    itinerary: {
+      type: "array",
+      items: [
+        {
+          type: "object",
+          properties: {
+            day: {
+              type: "string",
+            },
+            activities: {
+              type: "array",
+              items: [
+                {
+                  type: "object",
+                  properties: {
+                    time: {
+                      type: "string",
+                    },
+                    name: {
+                      type: "string",
+                    },
+                    location: {
+                      type: "string",
+                    },
+                    price: {
+                      type: "integer",
+                    },
+                    duration: {
+                      type: "string",
+                    },
+                    description: {
+                      type: "string",
+                    },
+                  },
+                  required: [
+                    "time",
+                    "name",
+                    "location",
+                    "price",
+                    "duration",
+                    "description",
+                  ],
+                },
+                {
+                  type: "object",
+                  properties: {
+                    time: {
+                      type: "string",
+                    },
+                    name: {
+                      type: "string",
+                    },
+                    location: {
+                      type: "string",
+                    },
+                    price: {
+                      type: "integer",
+                    },
+                    duration: {
+                      type: "string",
+                    },
+                    description: {
+                      type: "string",
+                    },
+                  },
+                  required: [
+                    "time",
+                    "name",
+                    "location",
+                    "price",
+                    "duration",
+                    "description",
+                  ],
+                },
+                {
+                  type: "object",
+                  properties: {
+                    time: {
+                      type: "string",
+                    },
+                    name: {
+                      type: "string",
+                    },
+                    location: {
+                      type: "string",
+                    },
+                    price: {
+                      type: "integer",
+                    },
+                    duration: {
+                      type: "string",
+                    },
+                    description: {
+                      type: "string",
+                    },
+                  },
+                  required: [
+                    "time",
+                    "name",
+                    "location",
+                    "price",
+                    "duration",
+                    "description",
+                  ],
+                },
+                {
+                  type: "object",
+                  properties: {
+                    time: {
+                      type: "string",
+                    },
+                    name: {
+                      type: "string",
+                    },
+                    location: {
+                      type: "string",
+                    },
+                    price: {
+                      type: "integer",
+                    },
+                    duration: {
+                      type: "string",
+                    },
+                    description: {
+                      type: "string",
+                    },
+                  },
+                  required: [
+                    "time",
+                    "name",
+                    "location",
+                    "price",
+                    "duration",
+                    "description",
+                  ],
+                },
+                {
+                  type: "object",
+                  properties: {
+                    time: {
+                      type: "string",
+                    },
+                    name: {
+                      type: "string",
+                    },
+                    location: {
+                      type: "string",
+                    },
+                    price: {
+                      type: "integer",
+                    },
+                    duration: {
+                      type: "string",
+                    },
+                    description: {
+                      type: "string",
+                    },
+                  },
+                  required: [
+                    "time",
+                    "name",
+                    "location",
+                    "price",
+                    "duration",
+                    "description",
+                  ],
+                },
+              ],
+            },
+          },
+          required: ["day", "activities"],
+        },
+        {
+          type: "object",
+          properties: {
+            day: {
+              type: "string",
+            },
+            activities: {
+              type: "array",
+              items: [
+                {
+                  type: "object",
+                  properties: {
+                    time: {
+                      type: "string",
+                    },
+                    name: {
+                      type: "string",
+                    },
+                    location: {
+                      type: "string",
+                    },
+                    price: {
+                      type: "null",
+                    },
+                    duration: {
+                      type: "null",
+                    },
+                    description: {
+                      type: "null",
+                    },
+                  },
+                  required: [
+                    "time",
+                    "name",
+                    "location",
+                    "price",
+                    "duration",
+                    "description",
+                  ],
+                },
+              ],
+            },
+          },
+          required: ["day", "activities"],
+        },
+      ],
+    },
+  },
+  required: ["itinerary"],
+});
+
+// const PROMPT = `Imagine you as a Trip planner, and plan a trip to PLACE for NUMBER_OF_DAYS days,
+//  of type TRIP_TYPE starting from TRIP_START in the BUDGET budget, plan all the trip of every day in the given format FORMAT,
+//   plan everything under BUDGET budget, Try to cover all important places in PLACE in NUMBER_OF_DAYS days of TRIP_TYPE type`
+const PROMPT = `You should always check if the place exist in the globe, if not then simply tell no place exist.
+User is going to place:PLACE, for number of days:NUMBER_OF_DAYS, for a this type of trip: TRIP_TYPE, user is
+starting the trip on TRIP_START, also the budget: BUDGET.
+Your task is to
+create a complete Trip Planner. Lets divide how you will give the trip planner step by step:
+1.) First check if the place exists where the user is wanting to go, if not then tell the user that this does not present in globe !,
+Then check where the user is going, for how many days and which type of trip the user is planning, about trip start and budget. Always be customised and personalised for the user.
+2.) Take time to think and plan the trip on your own first.
+3.) You should provide where the user will go on which day, on a single day time based trip planning, at what time user will visit at
+what site seeing area, also add a small comment about that area, where the user will have breakfast, shopping, dinner etc.
+4.) Final Output is the json in the mentioned format:FORMAT. No extra text needed.
+5.) Show all prices in INR.`;
+
+export const createTripPrompt = ({
+  where_to,
+  number_of_days,
+  itinerary_type,
+  trip_start = "",
+  budget = "",
+}) => {
+
+  let prompt =
+    `Strictly adhere to the mentioned JSON format` +
+    `FORMAT:${JSON_FORMAT},` +
+    `PLACE:${where_to}, NUMBER_OF_DAYS:${number_of_days},` +
+    ` TRIP_TYPE: ${itinerary_type},` +
+    ` TRIP_START: ${trip_start} and` +
+    `BUDGET:${budget}.` +
+    PROMPT;
+  
+  return prompt;
+};
